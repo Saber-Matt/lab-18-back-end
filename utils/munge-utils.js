fedff5edaa429e8d44ad4fc7ee245300ec431051
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-20 21:13:45 -0700

    Fix munge util bug and add https

--- utils/munge-utils.js
+++ utils/munge-utils.js
@@ -1,14 +1,23 @@
 export function formatShows(data) {
   
   return data.map(show => {
-
-    return {
-      showId: show.show.id,
-      title: show.show.name,
-      image: show.show.image.medium,
-      rating: show.show.rating.average,
-      description: show.show.summary
-    };
+    if(show.show.image){
+      return {
+        showId: show.show.id,
+        title: show.show.name,
+        image: show.show.image.medium,
+        rating: show.show.rating.average,
+        description: show.show.summary
+      };
+    } else {
+      return  {
+        showId: show.show.id,
+        title: show.show.name,
+        rating: show.show.rating.average,
+        description: show.show.summary
+      };
+    }
+    
   });
 
 }
